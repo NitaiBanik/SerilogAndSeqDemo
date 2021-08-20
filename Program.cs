76b74684990c0a6308e39ae690f9c0ad7eed3978
Nitai Chandra Banik <nitai.banik@selise.ch>
2021-08-21 01:21:29 +0600

    testing serilog configurations

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Serilog;
+using Serilog.Events;
+using System;
 
 namespace SerilogAndSeqDemo
 {
@@ -8,11 +10,28 @@ namespace SerilogAndSeqDemo
     {
         public static void Main(string[] args)
         {
-            using var log = new LoggerConfiguration()
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
                 .WriteTo.Console()
+                .WriteTo.File("logs/Serilog.txt", LogEventLevel.Debug, rollingInterval: RollingInterval.Minute)
                 .CreateLogger();
 
-            log.Information("Hello Nitai, from Serilog!");
+            int a = 10, b = 0;
+            try
+            {
+                Log.Debug("Dividing {A} by {B}", a, b);
+                Console.WriteLine(a / b);
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Something went wrong");
+            }
+            finally
+            {
+                Log.Information("Hello Nitai, from Serilog!");
+                Log.CloseAndFlush();
+            }
+
 
             CreateHostBuilder(args).Build().Run();
         }
